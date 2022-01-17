e48999839d05033beb0aef7419dd76b180aabd21
pk <piotr.m.klis@gmail.com>
2022-01-17 19:48:34 +0100

    fetch executives update

--- dags/utils/voting/fetch_executives.py
+++ dags/utils/voting/fetch_executives.py
@@ -1,48 +1,72 @@
+import requests
+from bs4 import BeautifulSoup
 from dags.connectors.sf import sf
-from dags.utils.voting.tooling.get_executives import get_execs
 
 
 def _fetch_executives(**setup):
 
-    executives = get_execs()
-
     sf_execs = sf.execute(
         f"""
-        SELECT code
+        SELECT code, title
         FROM {setup['votes_db']}.internal.yays
         WHERE type = 'executive'; """
     ).fetchall()
 
-    sf_execs = [executive[0] for executive in sf_execs]
+    codes = [executive[0] for executive in sf_execs]
     # adding addresses to omit (old DEFCON5 Disable the Liquidation Freeze spell and Activate DSChief v1.2)
-    sf_execs = sf_execs + [
+    codes = codes + [
         '0x0000000000000000000000000000000000000000',
         '0x02fc38369890aff2ec94b28863ae0dacdb2dbae3',
     ]
 
-    records = [
-        [
-            'executive',
-            executive['address'].lower().strip(),
-            executive['title'],
-            None,
-            None,
-            None,
-            None,
-            None,
-            None,
-        ]
-        for executive in executives
-        if executive['address'].lower().strip() not in sf_execs
-    ]
-    if len(records) > 0:
+    titles = [executive[1] for executive in sf_execs]
 
-        _execs = str()
-        counter = 0
-        for i in records:
-            _execs = _execs + str(i[1]) + ', '
-            counter += 1
+    url = f"https://api.github.com/repos/makerdao/community/contents/governance/votes"
+    r = requests.get(url)
+    res = r.json()
 
-    print(f"Executives titles: {len(executives)} read, {len(records)} prepared to upload")
+    records = list()
+    for r in res:
+        if r['type'] == 'file':
+
+            page = requests.get(r['html_url'])
+            soup = BeautifulSoup(page.content, "html.parser")
+            entry = soup.find(id="readme").find("article").find("table").find("tbody").find("tr").find_all("td")
+            
+            unresolved_title = entry[0].find("div").text
+            if "Template - [Executive Vote] " in unresolved_title:
+                
+                resolved = unresolved_title.replace("Template - [Executive Vote] ", "")
+                code = entry[3].find("div").text
+
+            elif len(entry) < 4:
+
+                resolved = unresolved_title
+                code = None
+
+            else:
+                
+                resolved = unresolved_title
+                code = entry[3].find("div").text
+            
+            if code:
+                if code.lower() not in codes:
+                    if resolved not in titles:
+                        records = [
+                            [
+                                'executive',
+                                code.lower(),
+                                resolved,
+                                None,
+                                None,
+                                None,
+                                None,
+                                None,
+                                None,
+                            ]
+                        ]
+
+            else:
+                pass
 
     return records
