03fe667f39746b28bbd12a8673cad231edb578a7
pk <piotr.m.klis@gmail.com>
2022-03-30 11:51:40 +0200

    dai_history human readable amount

--- dags/utils/history/dai_history.py
+++ dags/utils/history/dai_history.py
@@ -18,7 +18,7 @@ def update_dai_history(sf):
         select block, timestamp, tx_hash, 'DAI' as token,
             concat('0x', right(topic1, 40)) as sender,
             concat('0x', right(topic2, 40)) as receiver,
-            maker.public.etl_hextoint(log_data) as amount
+            maker.public.etl_hextoint(log_data) / power(10,18) as amount
         from edw_share.raw.events
         where contract = '0x6b175474e89094c44da98b954eedeac495271d0f' and
             topic0 = '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef' and
