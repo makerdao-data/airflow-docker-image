20ba5fcc27db0c9e284bf6261a2685404d5383d7
pk <piotr.m.klis@gmail.com>
2022-04-01 10:12:46 +0200

    unifying .beg and .pad parameters

--- dags/utils/parameters/load.py
+++ dags/utils/parameters/load.py
@@ -243,8 +243,8 @@ def _load(**setup):
             select block, timestamp, tx_hash, order_index,
             'FLAPPER.beg' as parameter,
             null as ilk,
-            maker.public.etl_hextoint(prev_value) / pow(10, 18) as from_value,
-            maker.public.etl_hextoint(curr_value) / pow(10, 18) as to_value
+            iff(maker.public.etl_hextoint(prev_value) = 0, 0, maker.public.etl_hextoint(prev_value) / pow(10, 18) - 1) as from_value,
+            iff(maker.public.etl_hextoint(curr_value) = 0, 0, maker.public.etl_hextoint(curr_value) / pow(10, 18) - 1) as to_value,
             from edw_share.raw.storage_diffs
             where contract = '0xc4269cc7acdedc3794b221aa4d9205f564e27f0d' and
             location = '4' and
@@ -275,8 +275,8 @@ def _load(**setup):
             when '5' then 'FLOPPER.pad'
             end as parameter,
             null as ilk,
-            maker.public.etl_hextoint(prev_value) / pow(10, 18) as from_value,
-            maker.public.etl_hextoint(curr_value) / pow(10, 18) as to_value
+            iff(maker.public.etl_hextoint(prev_value) = 0, 0, maker.public.etl_hextoint(prev_value) / power(10, 18) -1) as from_value,
+            iff(maker.public.etl_hextoint(curr_value) = 0, 0, maker.public.etl_hextoint(curr_value) / power(10, 18) -1) as to_value,
             from edw_share.raw.storage_diffs
             where contract = '0xa41b6ef151e06da0e34b009b86e828308986736d' and
             location in ('4', '5') and
