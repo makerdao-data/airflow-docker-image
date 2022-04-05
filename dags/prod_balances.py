20225cd4b620b16c88dd9730b3b9af6e6166cd80
leo <03ladr@gmail.com>
2022-04-05 17:37:27 -0400

    created script and dag

---
+++ dags/prod_balances.py
@@ -0,0 +1,51 @@
+import sys
+from datetime import datetime, timedelta
+
+from airflow.decorators import dag, task
+from dags.connectors.sf import connection as conn
+from dags.utils.balances import update_token_balances
+
+sys.path.append('/opt/airflow/')
+
+# [START default_args]
+# These args will get passed on to each operator
+# You can override them on a per-task basis during operator initialization
+default_args = {
+    "owner": "airflow",
+    "email": "airflow@data.makerdao.network",
+    "email_on_failure": True,
+    "retries": 0,
+    "retry_delay": timedelta(minutes=1),
+}
+# [END default_args]
+
+
+# [START instantiate_dag]
+@dag(
+    default_args=default_args,
+    schedule_interval='0 0 * * *',
+    start_date=datetime(2022, 5, 0),
+    max_active_runs=1,
+    catchup=False,
+)
+def prod_bals():
+
+    @task()
+    def update_dai_balances():
+
+        update_token_balances('DAI', conn)
+
+        return
+
+    @task()
+    def update_mkr_balances():
+
+        update_token_balances('MKR', conn)
+
+        return
+
+    update_dai_balances()
+    update_mkr_balances()
+
+
+prod_bals = prod_bals()
