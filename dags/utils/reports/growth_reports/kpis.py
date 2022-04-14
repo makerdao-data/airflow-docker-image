d661cb7c6b6ff2dab93fccf2bb059010ed64dc57
leo <03ladr@gmail.com>
2022-04-14 09:20:16 -0400

    added datetime import

--- dags/utils/reports/growth_reports/kpis.py
+++ dags/utils/reports/growth_reports/kpis.py
@@ -4,7 +4,7 @@ Automating Growth KPIs google sheets report.
 import calendar
 import sys
 import requests
-from datetime import date
+from datetime import date, datetime
 from typing import List, Tuple
 
 import pandas as pd
